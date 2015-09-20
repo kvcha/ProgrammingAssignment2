47779f7521c46bc7ddf7552b48c324dd7327d857
kvcha <mailer87@gmail.com>
2015-09-21 00:23:28 +0300

    Functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This functions expanding classic matrix function, adding possibility to lookup cache for return of matrix. 
 
-## Write a short comment describing this function
+## Seting default matrix, binding "result" variable, making list of operating functions.    
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x <- matrix()) {
+
+  # Empty value 
+        result <- NULL
+        set <- function(y)  {
+  # <<- helps to assign value for variable, "from outside", from another enviroment. 
+          x <<- y
+          result <<- NULL
+        }
+  #making list of operating functions
+        get <- function() x
+        setresult <- function(inverse) result <<- inverse
+        getresult <- function() result
+        list(set = set, get = get, setresult = setresult, getresult = getresult)
 
 }
 
 
-## Write a short comment describing this function
+## This function has 3 parts: 1. Taking output form previous function. 2. Making check if result already in cache. 3. If it is not - performing calculations. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+#link to previous function
+       result <- x$getresult() result
+  
+  #chekcing cache for results
+       if (!is.null(result)) {
+          message("......readaing cache......getting data from cache.......")
+          return(result)
+  }
+  #if result = NULL, run calculations
+      matxdata <- x$get()
+  #now we should use our key function
+      result <- solve(matxdata, ...)
+  
+  #that`s all, now lets drop result to cache
+      x$setresult(result)
+  
+  #print
+  return(result)
+
 }
